e1341ab7f24928f3a96e713453c6221b75645b5e
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-06-29 08:34:14 +0530

    Removed Unnecessary init()

--- dist/search.d.ts
+++ dist/search.d.ts
@@ -9,10 +9,8 @@ export declare class Search implements SearchOptions {
     data: any;
     keys: Array<string>;
     filteredData: Array<object>;
-    initialized: boolean;
     params: any;
     stopwords: any;
-    init(...params: any[]): void;
     add(...addKey: any): void;
     search(searchKey: string): object[];
 }
--- src/search.ts
+++ src/search.ts
@@ -1,76 +1,55 @@
-interface SearchOptions{
+interface SearchOptions {
     params: {
         data: any,
         exclude ? : any
     }
 }
 
-export class Search implements SearchOptions{
-    constructor(params: any){
+export class Search implements SearchOptions {
+    constructor(params: any) {
         this.params = params;
         this.keys = [];
         this.filteredData = [];
-        this.initialized = false;
         this.stopwords;
-    }
-    data: any;
-    keys: Array<string>;
-    filteredData: Array<object>;
-    initialized: boolean;
-    params: any;
-    stopwords: any;
-    
 
-    init(...params){
-        // Conditions for empty/null input
-        if(!this.params.data) throw "Undefined or null data provided.";
-        if(!Object.keys(this.params.data).length) throw "Can't search inside an empty variable";
-
-        // Set initialized variable to be true
-        this.initialized = true;
-
-        // Setting stopwords
-        if(this.params.exclude){
+        // Set the dictionary
+        if (this.params.exclude) {
             this.stopwords = new Set(this.params.exclude);
         }
-        // console.log(this.stopwords);
-
-        // **DEBUG**
-        // console.log(searchData);
     }
+    data: any;
+    keys: Array < string > ;
+    filteredData: Array < object > ;
+    params: any;
+    stopwords: any;
 
-    add(...addKey: any){
+    add(...addKey: any) {
         // Conditions for empty/null input
-        if(!addKey) throw "Key not provided";
-
-        // Condition for uninitialized
-        if(!this.initialized) throw "Class not initialized."
+        if (!addKey) throw "Key not provided";
 
         // Adding keys
         addKey.forEach(key => {
             this.keys.push(key);
         })
-       
+
     }
 
-    search(searchKey: string){
+    search(searchKey: string) {
         // Conditions for empty/null
-        if(!searchKey) throw "Key not provided";
+        if (!searchKey) throw "Key not provided";
 
-        // Condition for uninitialized
-        if(!this.initialized) throw "Class not initialized."
         // console.log(this.data, this.keys);
 
         // Stopwords filtering function
         const filterStopwords = (word: string) => {
-            if(this.stopwords.has(word)){
+            if (this.stopwords.has(word)) {
                 return true;
             }
             return false;
         }
 
-        // Actual Search logic
-        this.params.data.filter(item => {
+          // Actual Search logic
+          this.params.data.filter(item => {
             this.keys.some(key => {
                 const filteredItem = item[key].toLowerCase();
                 const filteredSearchKey = searchKey.toLowerCase();
@@ -85,7 +64,7 @@ export class Search implements SearchOptions{
                 }
             })
         })
-        
+
         return this.filteredData;
 
     }
